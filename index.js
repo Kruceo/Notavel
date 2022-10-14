3e677f2de00b37d37a1b0f6382095298ecc26fbc
Kruceo <rafa2015krueger@gmail.com>
2022-10-14 12:18:47 -0300

    fix: all text higlight

--- index.js
+++ index.js
@@ -1,61 +1,98 @@
+import hljs from "highlight.js";
 document.querySelectorAll("code-editor").forEach((ce) => {
-  console.log(ce);
+  const initialValue = ce.innerHTML
+  ce.innerHTML = '';
   let tst = ce;
   let lineCounterPad = 2;
+  let lang = "js";
   console.log(tst.style);
-  let textEditorEl = document.createElement("p");
+  let textEditorEl = document.createElement("code");
+  
   let header = document.createElement("header");
   textEditorEl.setAttribute("contenteditable", "true");
-  let rowsCountEl = document.createElement("p");
+  let rowsCountEl = document.createElement("code");
   let rowsCountTxt = "";
   tst.style.display = "block";
   tst.style.overflow = "hidden";
   header.style.display = "flex";
   header.style.margin = 0;
-  header.style.overflow = "auto";
+  header.style.overflowY = "auto";
+  header.style.overflowX = "none";
   header.style.height = "100%";
   rowsCountEl.style = "background-color: #00000050;margin:0 0 0 0;padding: 0";
-  rowsCountEl.style.color = getComputedStyle(tst).getPropertyValue("--counter-color");
+  rowsCountEl.style.color =
+    getComputedStyle(tst).getPropertyValue("--counter-color");
   rowsCountEl.style.minHeight = "100%";
   rowsCountEl.style.height = "fit-content";
   rowsCountEl.style.paddingInline = "10px";
   textEditorEl.style.width = "100%";
+  textEditorEl.style.overflowWrap= "normal";
+  textEditorEl.style.setProperty('-webkit-user-modify', 'read-write-plaintext-only')
   textEditorEl.style.height = "fit-content";
   textEditorEl.style.minHeight = "100%";
-  textEditorEl.style.overflowY = "hidden";
+  textEditorEl.style.overflow = "hidden hidden";
   textEditorEl.style.margin = "0";
   textEditorEl.style.whiteSpace = "nowrap";
-  textEditorEl.style.overflowX = "overlay"
+  textEditorEl.tabIndex = 0;
+  // textEditorEl.style.overflowX = "overlay";
   textEditorEl.style.paddingInline = "10px";
   textEditorEl.style.border = "0";
   textEditorEl.style.outline = "none";
-  
-  textEditorEl.value = tst.value;
-  textEditorEl.addEventListener("input", (e) => {
-    rowsCountTxt = "";
-    let rows = [...textEditorEl.innerHTML.matchAll("<div>")];
+  textEditorEl.innerHTML = initialValue;
 
-    for (let i = 0; i < rows.length + 1; i++) {
-      rowsCountTxt += (i + 1).toString().padStart(lineCounterPad, "0") + "\n";
-    }
-    rowsCountEl.innerText = rowsCountTxt;
+  //start
+  genRowsCounter(textEditorEl,rowsCountEl)
+  genHighlight(textEditorEl);
+
+
+console.log(getComputedStyle(textEditorEl));
+
+  textEditorEl.addEventListener("input", (e) => {
     tst.value = textEditorEl.innerText;
+    genRowsCounter(textEditorEl,rowsCountEl);
+    //genHighlight(textEditorEl,'javascript')
   });
 
   textEditorEl.addEventListener("focusout", (e) => {
-    try {
-      if (hljs != undefined && hljs != null) {
-        hljs.highlightElement(textEditorEl);
-      }
-    } catch (err) {
-      console.info("try using with highlight.js");
-    }
+   genHighlight(textEditorEl);
   });
-  //let style = document.createElement('style');
-  //style.innerHTML = "pre code.hljs{display:block;overflow-x:auto;padding:1em}code.hljs{padding:3px 5px}.hljs{color:#000;background:#fff}.hljs-subst,.hljs-title{font-weight:400;color:#000}.hljs-title.function_{color:#7a7a43}.hljs-code,.hljs-comment,.hljs-quote{color:#8c8c8c;font-style:italic}.hljs-meta{color:#9e880d}.hljs-section{color:#871094}.hljs-built_in,.hljs-keyword,.hljs-literal,.hljs-meta .hljs-keyword,.hljs-name,.hljs-selector-attr,.hljs-selector-class,.hljs-selector-id,.hljs-selector-pseudo,.hljs-selector-tag,.hljs-symbol,.hljs-template-tag,.hljs-type,.hljs-variable.language_{color:#0033b3}.hljs-attr,.hljs-property{color:#871094}.hljs-attribute{color:#174ad4}.hljs-number{color:#1750eb}.hljs-regexp{color:#264eff}.hljs-link{text-decoration:underline;color:#006dcc}.hljs-meta .hljs-string,.hljs-string{color:#067d17}.hljs-char.escape_{color:#0037a6}.hljs-doctag{text-decoration:underline}.hljs-template-variable{color:#248f8f}.hljs-addition{background:#bee6be}.hljs-deletion{background:#d6d6d6}.hljs-emphasis{font-style:italic}.hljs-strong{font-weight:700}"
-  //document.body.appendChild(style);
+  let style = document.createElement("style");
+  style.innerHTML =
+    `
+    pre code.hljs{display:block;overflow-x:auto;padding:1em}code.hljs{padding:3px 5px}.hljs{background:#222;color:#aaa}.hljs-subst{color:#aaa}.hljs-section{color:#fff}.hljs-comment,.hljs-meta,.hljs-quote{color:#444}.hljs-bullet,.hljs-regexp,.hljs-string,.hljs-symbol{color:#fc3}.hljs-addition,.hljs-number{color:#0c6}.hljs-attribute,.hljs-built_in,.hljs-link,.hljs-literal,.hljs-template-variable,.hljs-type{color:#32aaee}.hljs-keyword,.hljs-name,.hljs-selector-class,.hljs-selector-id,.hljs-selector-tag{color:#64a}.hljs-deletion,.hljs-template-tag,.hljs-title,.hljs-variable{color:#b16}.hljs-doctag,.hljs-section,.hljs-strong{font-weight:700}.hljs-emphasis{font-style:italic}
+    `;
+ document.body.appendChild(style);
 
   tst.appendChild(header);
   header.appendChild(rowsCountEl);
   header.appendChild(textEditorEl);
 });
+
+function genHighlight(codeElement,lang)
+{
+  try {
+    if (hljs != undefined && hljs != null) {
+      const rawContent = codeElement.innerText;
+      codeElement.innerHTML = '';
+      rawContent.split('\n').forEach((each)=>
+      {
+        codeElement.innerHTML +=   hljs.highlight(each,{language: 'html'}).value + '<br>';
+      }) 
+    }
+  } catch (err) {
+    //console.info("Try using with highlight.js");
+    console.log(err);
+  }
+}
+
+function genRowsCounter(source,targetElement)
+{
+  let rowsCountTxt = "";
+  
+  let rows = source.innerText.split('\n');
+  for (let i = 0; i < rows.length; i++) {
+    rowsCountTxt += (i + 1).toString().padStart(3,0) + "<br>";
+  }
+  targetElement.innerHTML = rowsCountTxt;
+  
+}
